58e94a7758e3fd1eb3e77fa37316ca1a7a8c1675
nobodywow <krest548@gmail.com>
2019-09-24 02:31:41 +0300

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync');
+
+const server = browserSync.create();
+
+const paths = {
+    less: 'src/**/**/*.less'
+};
+
+const scripts = () => gulp.src(paths.less, { sourcemaps: true })
+    .pipe(concat('style.css'))
+    .pipe(less())
+    .pipe(gulp.dest('./public'))
+    .pipe(browserSync.stream());
+
+const serve = (done) => {
+    server.init({
+        server: {
+            baseDir: './public'
+        }
+    });
+    done();
+};
+
+const reload = (done) => {
+    server.reload();
+    done();
+};
+
+const watch = () => gulp.watch(paths.less, gulp.series(scripts, reload));
+
+const dev = gulp.series(scripts, serve, watch);
+
+exports.default = dev;
